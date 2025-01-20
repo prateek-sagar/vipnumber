8237c739bad6b029994fd362276df9a2b8e4e1b0
Prateek <prateeksagar806@gmail.com>
2025-01-20 08:20:30 +0530

    Written tests for Number Entry Class

--- src/main/java/com/vipnumberweb/vipnumber/model/entities/NumberEntry.java
+++ src/main/java/com/vipnumberweb/vipnumber/model/entities/NumberEntry.java
@@ -1,8 +1,23 @@
 package com.vipnumberweb.vipnumber.model.entities;
 
-import jakarta.persistence.Entity;
+// import jakarta.persistence.Entity;
 
-@Entity
+// @Entity
 public class NumberEntry {
+    private int[] entry;
 
+    public NumberEntry() {
+        entry = new int[10];
+    }
+
+    public void setEntryNumber(int[] arr) {
+        if (arr.length != 10)
+            throw new IllegalArgumentException();
+
+        entry = arr;
+    }
+
+    public int[] getEntryNumber() {
+        return entry;
+    }
 }
--- src/test/java/com/vipnumberweb/vipnumber/entitiestest/NumberEntryTest.java
+++ src/test/java/com/vipnumberweb/vipnumber/entitiestest/NumberEntryTest.java
@@ -1,13 +1,44 @@
 package com.vipnumberweb.vipnumber.entitiestest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.vipnumberweb.vipnumber.model.entities.NumberEntry;
+
 @SpringBootTest
 public class NumberEntryTest {
+    private NumberEntry numberEntry = new NumberEntry();
 
     @Test
     public void existenceOfClass() {
-
+        assertInstanceOf(NumberEntry.class, numberEntry);
     }
+
+    @Test
+    public void setEntryNumber_doesNotThrowException() {
+        int[] arr = new int[10];
+        assertDoesNotThrow(() -> numberEntry.setEntryNumber(arr));
+    }
+
+    @Test
+    public void setEntryNumber_throwException() {
+        int[] arr = new int[9];
+        assertThrows(IllegalArgumentException.class, () -> numberEntry.setEntryNumber(arr));
+    }
+
+    @Test
+    public void setEntry_getEntry() {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 0, 7, 8, 9 };
+        numberEntry.setEntryNumber(arr);
+        int[] newArray = numberEntry.getEntryNumber();
+
+        assertEquals(arr, newArray);
+    }
+
+    
 }
